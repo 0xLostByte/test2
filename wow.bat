65ce85d629211b832f13c854e8e4ce14640205a6
0xLostByte <loayslaw123454@gmail.com>
2026-09-18 02:28:28 +0300

    Update wow.bat

--- wow.bat
+++ wow.bat
@@ -1,1 +1,1 @@
-https://share.gemini.google/Gxkf5OL6qavc
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
